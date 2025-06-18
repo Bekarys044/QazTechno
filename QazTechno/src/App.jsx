a4785cb2749d20db48d6b700f0b4a1b523d9d313
Bekarys044 <dairovbekarys0406@gmail.com>
2025-06-18 19:18:58 +0500

    fixed som code

--- QazTechno/src/App.jsx
+++ QazTechno/src/App.jsx
@@ -1,5 +1,5 @@
 
-import {  HashRouter as Router, Route, Routes } from 'react-router-dom';
+import {  BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
